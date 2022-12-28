3715736c1c60e96dd9a2b0a4e9bfa7850664e4e3
Cenachi <carloscenachi@protonmail.com>
2022-12-27 22:58:53 -0300

    fcfs

--- src/App.java
+++ src/App.java
@@ -3,13 +3,15 @@ import models.Processos;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Processos p1 = new Processos("Processo 1",4, true,0);
-        Processos p2 = new Processos("Processo 2",5, false,0);
-        Processos p3 = new Processos("Processo 3",7, false,0);
+        Processos p1 = new Processos("Processo 1", 4, true, 0);
+        Processos p2 = new Processos("Processo 2", 5, false, 0);
+        Processos p3 = new Processos("Processo 3", 7, false, 0);
 
         ProcessosController pController = new ProcessosController();
-        System.out.println("Iniciando processo FCFS:"); 
+        System.out.println("Iniciando escalonamento FCFS:");
         pController.fcfs(p1, p2, p3);
 
+        System.out.println("Iniciando escalonamento SJF"); 
+        pController.sjf(p1, p2, p3);
     }
 }
--- src/controller/ProcessosController.java
+++ src/controller/ProcessosController.java
@@ -10,12 +10,9 @@ public class ProcessosController {
     static Random random = new Random();
 
     public static void fcfs(Processos p1, Processos p2, Processos p3) throws InterruptedException {
-
         filaProcessos.add(p1);
         filaProcessos.add(p2);
         filaProcessos.add(p3);
-
-        int processo = 1;
 
         while (!filaProcessos.isEmpty()) {
 
@@ -25,20 +22,23 @@ public class ProcessosController {
                 int interrupted = random.nextInt(p.getTime()) + 1;
 
                 for (int j = 0; j < interrupted; j++) {
-                    System.out.print("\r"+ p.getName()+" em: " + j + "s");
+                    System.out.print("\r" + p.getName() + " em: " + j + "s");
                     Thread.sleep(1000);
                 }
                 filaProcessos.add(new Processos(p.getName(), p.getTime(), false, interrupted));
 
             } else {
                 for (int i = p.getTimeSpent(); i <= p.getTime(); i++) {
-                    System.out.print("\r" +p.getName() + " em: " + i + "s");
+                    System.out.print("\r" + p.getName() + " em: " + i + "s");
                     Thread.sleep(1000);
                 }
             }
             System.out.println();
-            processo++;
         }
+    }
+
+    public static void sjf(Processos p1, Processos p2, Processos p3) throws InterruptedException {
 
     }
+
 }
